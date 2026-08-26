1fe7b175bd339b6e41e88d0272403e461e9f4527
Le Huong Giang <hgiangle.work@gmail.com>
2026-08-26 11:16:41 +0700

    feat: add medium font weight and consolidate script typography tokens with updated section styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ const season = localFont({
     { path: "./fonts/season-serif-light.woff2", weight: "300", style: "normal" },
     { path: "./fonts/season-serif-light-italic.woff2", weight: "300", style: "italic" },
     { path: "./fonts/season-serif-regular.woff2", weight: "400", style: "normal" },
+    { path: "./fonts/season-serif-medium.woff2", weight: "500", style: "normal" },
   ],
   variable: "--font-season",
   display: "swap",
